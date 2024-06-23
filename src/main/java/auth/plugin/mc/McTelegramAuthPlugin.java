de6aa47581b4238078a17cce655e3f9f3d6dd77f
hottabych04 <hottabych04@gmail.com>
2024-06-24 01:50:06 +0300

    Init commit

---
+++ src/main/java/auth/plugin/mc/McTelegramAuthPlugin.java
@@ -0,0 +1,18 @@
+package auth.plugin.mc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class McTelegramAuthPlugin extends JavaPlugin {
+
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+}
